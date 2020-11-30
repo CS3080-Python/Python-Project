9ea7bd6a3a0ab6484b502f6204e5ab7755e31128
flashtrance55 <flashtrance55@gmail.com>
2020-11-30 10:44:08 -0700

    Added ability to search database for topics. Added check for existing documents to make sure they are updated if a search on an existing topic is conducted. Fixed misc. typos, adjusted some formatting.

--- dataflow/get_data.py
+++ dataflow/get_data.py
@@ -4,16 +4,16 @@ import json
 # Define functions for each view type
 def get_topic_data(topic):
   url = 'http://localhost:3000/design/topics/' + topic
-  get_data(url)
+  return get_data(url)
 
 def get_saType_data(sa_type):
   url='http://localhost:3000/design/sa_types/' + sa_type
-  get_data(url)
+  return get_data(url)
 
 # General function to retrieve data from database given a valid URL
 def get_data(url):
   res = requests.get(url)
-  
+
   if res.status_code != 200:
     print("Error:", res.status_code)
 
